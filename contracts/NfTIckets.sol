19ddb5f46374de9049a944b35a33d5f3657a2a6d
rachosuar <rachosuar@gmail.com>
2022-12-10 21:04:32 -0300

    transfer event added

--- contracts/NfTIckets.sol
+++ contracts/NfTIckets.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/interfaces/IERC721.sol";
 
 contract NFTtrade{
+event nftTransfer(address _to, uint256 id);
 // state variables
 
 //set Airline and TravelX addresses
 
 //set a mapping to save timestamp for transfer deadline for each NFT.
 
-//
+//set a function that allow to set the price of NFT so it can be selled
 
 
 
